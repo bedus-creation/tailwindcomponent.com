ba95d93dbb4b8d6940d50f773f99849ba452e8f5
Ellite <tmgbedu@gmail.com>
2020-05-06 12:42:53 +0545

    [Fix] Landing page changed. :art:

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageSeoController;
 use App\Http\Controllers\DocumentController;
 
-Route::redirect('/', '/component/alert');
+Route::redirect('/', '/component/introduction');
 Route::get('component/{component}', [DocumentController::class, 'index']);
 Route::resource('admin/pageseo', PageSeoController::class);
