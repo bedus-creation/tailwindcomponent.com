9f41f00e633e97aeeba552a10762614cf00c62a0
Ellite <tmgbedu@gmail.com>
2020-05-06 07:43:25 +0545

    [Seo] Google analytics code has been added. :label:

--- resources/views/theme/docx/app.blade.php
+++ resources/views/theme/docx/app.blade.php
@@ -42,6 +42,14 @@
     <script>
         hljs.initHighlightingOnLoad();
     </script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106586554-6"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-106586554-6');
+    </script>
 </body>
 
 </html>
