d1622ab1724206dcef96ac1f6395ff8cc5b715cb
Ellite <tmgbedu@gmail.com>
2020-05-07 20:21:32 +0545

    [SEO] Google analytics

--- resources/views/theme/editor/app.blade.php
+++ resources/views/theme/editor/app.blade.php
@@ -19,6 +19,14 @@
     </div>
     <script src="/js/app.js"></script>
     @yield('scripts')
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106586554-6"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-106586554-6');
+    </script>
 </body>
 
 
