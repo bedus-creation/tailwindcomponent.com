961103e80917f9ee262ea630d69b0d34b1784c59
Ellite <tmgbedu@gmail.com>
2020-05-08 16:35:59 +0545

    interia added

---
+++ resources/js/components/ActionButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Button
+      type="button"
+      @click="show= !show"
+      class="fixed bg-blue-600 text-gray-200 rounded-full px-6 py-4 z-20"
+      style="bottom:20px; right:20px"
+    >+</Button>
+    <div
+      v-show="show"
+      class="fixed top-0 right-0 h-screen bg-gray-100 p-8 shadow z-10"
+      style="width:24rem"
+    >
+      <design-create-form></design-create-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    };
+  }
+};
+</script>
---
+++ resources/js/components/DesignCreateForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form>
+    <div class="mb-2">
+      <label class="text-gray-700 font-light">Gist Title</label>
+      <input type="text" class="w-full border rounded px-3 py-1 text-gray-700" />
+    </div>
+    <div class="mb-2">
+      <label class="text-gray-700 font-light">Catgories</label>
+      <select class="w-full border bg-white rounded py-2">
+        <option>Option 1</option>
+        <option>Option 2</option>
+        <option>Option 3</option>
+      </select>
+    </div>
+    <div>
+      <label class="text-gray-700 font-light">Tell us more</label>
+      <textarea class="w-full border rounded" rows="4"></textarea>
+
+      <button
+        type="button"
+        class="bg-green-500 text-white rounded px-3 py-2 text-sm float-right"
+      >Save Gist</button>
+    </div>
+  </form>
+</template>
