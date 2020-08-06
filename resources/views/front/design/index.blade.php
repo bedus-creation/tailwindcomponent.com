de6e0b3719174aae3ef4a775f270d81521610753
Ellite <tmgbedu@gmail.com>
2020-08-06 23:31:39 +0545

    [Fix] Design page ui update.

--- resources/views/front/design/index.blade.php
+++ resources/views/front/design/index.blade.php
@@ -32,7 +32,7 @@ Tailwind Css layout and template design examples.
     <div class="w-full mb-4 px-4 md:w-1/3">
         <div class="h-full rounded-lg shadow-lg" style="background-color:hsl(0, 0%, 100%);">
             <a target="_blank" href="{{$item->link()}}" class="mb-4">
-                <div class="w-full h-40"
+                <div class="rounded-t-lg w-full h-40"
                     style="background: url({{url(optional($item->pageseo)->cover ?? ' ')}}); background-size:cover !important; background-position:center !important">
                 </div>
             </a>
