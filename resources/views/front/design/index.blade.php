bcc5e31febe46c1fb1957eee2368d1a7d3fa052e
Ellite <tmgbedu@gmail.com>
2020-08-06 23:30:22 +0545

    [Fix] Design page ui update.

--- resources/views/front/design/index.blade.php
+++ resources/views/front/design/index.blade.php
@@ -29,14 +29,14 @@ Tailwind Css layout and template design examples.
 @section('content')
 <div class="md:flex flex-wrap -mx-4">
     @foreach($design as $item)
-    <div class="w-full mb-2 px-4 md:w-1/3">
-        <div class="h-full rounded-lg" style="background-color:hsl(0, 0%, 100%);">
+    <div class="w-full mb-4 px-4 md:w-1/3">
+        <div class="h-full rounded-lg shadow-lg" style="background-color:hsl(0, 0%, 100%);">
             <a target="_blank" href="{{$item->link()}}" class="mb-4">
-                <div class="rounded w-full h-40"
+                <div class="w-full h-40"
                     style="background: url({{url(optional($item->pageseo)->cover ?? ' ')}}); background-size:cover !important; background-position:center !important">
                 </div>
             </a>
-            <div class="px-4 py-4">
+            <div class="border-t border-gray-100 px-4 py-4">
                 <div class="mb-2">
                     <a target="_blank" href="{{$item->link()}}"
                         class="font-semibold leading-tight text-xl text-gray-800 hover:text-gray-800">
