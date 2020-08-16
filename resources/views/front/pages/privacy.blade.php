5c6dfc9cc1baafe0bedfbe481b75b62190620ac4
Bedram Tamang <tmgbedu@gmail.com>
2020-08-16 23:56:16 +0545

    [Update] Privacy updated

--- resources/views/front/pages/privacy.blade.php
+++ resources/views/front/pages/privacy.blade.php
@@ -3,7 +3,13 @@
 @section('content')
 <h1 class="text-gray-700 font-bold text-2xl md:text-3xl leading-snug">Privacy and Policy</h1>
 <p class="my-4 leading-relaxed text-gray-700">
-    Anything we create here in web2tailwind editor is public by default, and all the editor will indexed for public
-    search, findable.
+    Our team genuinely writes all the articles and codes by our self. We spent hours and hours to research and code in
+    Web2tailwind.com. If any posts or contents makes you feel uncomfortable to you, then feel free to reach me at <a
+        href="mailto:tmgbedu@gmail.com">tmgbedu@gmail.com</a>
+</p>
+<p class="my-4 leading-relaxed text-gray-700">
+    Besides that Web2tailwind.com is a public platform and you can create and store your code in web2tailwind.com which
+    is private by default. Private means that the content url contains hashed value which is really hard to guess and
+    the content will not availble in search and it will not indexed.
 </p>
 @endsection
