f868782ec87111812eb0a94b1b24536c15efdf2c
Ellite <tmgbedu@gmail.com>
2021-01-14 19:22:49 +0545

    - [x] Code Refactor :tada:

---
+++ resources/views/theme/core/components/scripts.blade.php
@@ -0,0 +1,12 @@
+@production
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106586554-6"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-106586554-6');
+    </script>
+    <script data-ad-client="ca-pub-4419475170841205" async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+@endproduction
