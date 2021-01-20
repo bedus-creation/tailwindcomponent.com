f93604bd709667e1bc914be422a8f064bff2e89f
Ellite <tmgbedu@gmail.com>
2021-01-20 20:31:33 +0545

    - [x] Preparing for merge.

--- bin/merge.sh
+++ bin/merge.sh
@@ -1,5 +1,5 @@
-echo "/public\/dist" >> .gitignore
-echo "/public\/mix-manifest.json" >> .gitignore
+echo "/public/dist" >> .gitignore
+echo "/public/mix-manifest.json" >> .gitignore
 
 git rm -r --cache .
 git add . && git commit -m "- [x] Preparing for merge."
